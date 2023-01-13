f19cfe37ba762db48acf0fad0ef32b7eae6ff638
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2023-01-13 15:09:24 +0530

    Added file operations

--- demoServer.js
+++ demoServer.js
@@ -1,22 +1,49 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req,res)=>{
+    let url = req.url;
+    let method = req.method;
     console.log('Welcome Pratyush');
-    
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Demo Server</title></head>');
-    
-    switch(req.url){
-        case '/home': res.write('<h1>Welcome to Home</h1>');
-        break;
-        case '/about': res.write('<h1>Welcome to About Us page</h1>');
-        break;
-        case '/node': res.write('<h1>Welcome to my NodeJS project</h1>');
-        break;
-        default: res.write('<h1>Welcome Pratyush</h1>');
+
+    if(url === '/'){
+        fs.readFile('response.txt','utf8', (err,data)=>{
+            res.write('<html>');
+            res.write('<head><title>Demo Server</title></head>');
+            res.write('<h1>Enter a message</h1>')
+            res.write(`<strong>${data}</strong>`);
+            res.write(`<body><form action="/message" method="POST">
+                        <input type="text" name="message"/>
+                        <button>Send</button>
+                    </form></body>`); 
+            res.write('</html>');
+            if(err){
+                console.log(err);   
+            }
+            return res.end();
+        });
     }
-    res.end();
+    
+    else if(url === '/message' && method==='POST'){
+        let body = []
+        req.on('data', (chunk)=>{
+            body.push(chunk);
+        })
+        req.on('end', ()=>{
+            let parsed = Buffer.concat(body).toString().split('=')[1];
+            fs.writeFile('response.txt', parsed, (err)=>{
+                if(err) console.log(err);
+            });
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    else{
+        res.setHeader('Content-Type', 'text/html');
+        return res.end();
+    }
 })
 
 server.listen(4000);
