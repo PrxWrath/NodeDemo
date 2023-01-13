4ee183983aeeadd2761f84dc5f51caa7f1fbf9fb
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2023-01-13 13:12:49 +0530

    Added responses

--- demoServer.js
+++ demoServer.js
@@ -2,6 +2,21 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log('Welcome Pratyush');
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Demo Server</title></head>');
+    
+    switch(req.url){
+        case '/home': res.write('<h1>Welcome to Home</h1>');
+        break;
+        case '/about': res.write('<h1>Welcome to About Us page</h1>');
+        break;
+        case '/node': res.write('<h1>Welcome to my NodeJS project</h1>');
+        break;
+        default: res.write('<h1>Welcome Pratyush</h1>');
+    }
+    res.end();
 })
 
 server.listen(4000);
