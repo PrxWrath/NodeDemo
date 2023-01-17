ddc86efbeefb09b7a843bcd858afab37fecd927c
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2023-01-17 13:05:16 +0530

    Added request handling and body parser

--- express-demo/app.js
+++ express-demo/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use((req,res,next)=>{
-    console.log('First Middleware!');
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product', (req,res,next)=>{
+  res.send(`<form action="/product" method="POST">
+    <label>Title</label>
+    <input type="text" name="title"/>
+    <label>Size</label>
+    <input type="text" name="size"/>
+    <button>Add Product</button>
+  </form>`)
+})
+app.post('/product', (req,res,next)=>{
+  console.log(req.body);
+  res.redirect("/");
 })
 
-app.use((req,res,next)=>{
-    console.log("Second Middleware!");
+app.use('/', (req,res,next)=>{
     res.send('<h1>Hello From Express!!</h1>');
 })
 
