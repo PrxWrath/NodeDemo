dcdc04e884db6c895d449ea6c89055b33ebf470f
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2023-01-18 19:19:22 +0530

    Added edit and delete functionality

--- express-dynamic-routes/controllers/admin.js
+++ express-dynamic-routes/controllers/admin.js
@@ -15,7 +15,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -29,3 +29,37 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product=>{
+    if(!product){
+      res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      product
+    });
+  })
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  let updated = new Product(
+    prodId,
+    req.body.title,
+    req.body.imageUrl,
+    req.body.description,
+    req.body.price
+  )
+  updated.save();
+  res.redirect('/admin/products');
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteProductById(prodId);
+  res.redirect('/admin/products');
+}
+
--- express-dynamic-routes/models/product.js
+++ express-dynamic-routes/models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,17 +27,36 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if(this.id){
+        const existingIndex = products.findIndex(p => p.id===this.id);
+        const newProducts  = [...products];
+        newProducts[existingIndex] = this;
+        fs.writeFile(p, JSON.stringify(newProducts), err => {
+          console.log(err);
+        });
+      }else{
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
+  static deleteProductById(id) {
+    getProductsFromFile(products=>{
+      let deleted = products.filter(p=>p.id!==id);
+      fs.writeFile(p, JSON.stringify(deleted), err => {
+        console.log(err);
+      });
+    })
+  }
+  
   static fetchAll(cb) {
     getProductsFromFile(cb);
+    
   }
 
   static findById(id, cb) {
