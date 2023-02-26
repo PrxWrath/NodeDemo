8705281bab75cf81e6a536c14b8cc7a85a839993
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2023-02-26 13:07:16 +0530

    Integrated CRUD operations with MongoDB

--- node-nosql-shop/models/product.js
+++ node-nosql-shop/models/product.js
@@ -1,19 +1,54 @@
 const getDb = require('../util/database').getDb
+const mongoDB = require('mongodb');
 
 class Product{
-  constructor(title, price, description, imageUrl){
+  constructor(title, price, description, imageUrl, id){
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id
   }
 
   save(){
     const db = getDb();
-    return db.collection('products').insertOne(this)
-    .then(result=>{
-      console.log(result);
+    let op;
+    if(this._id){
+      op = db.collection('products').updateOne({_id: new mongoDB.ObjectId(this._id)}, {$set: this})
+    }else{
+      op = db.collection('products').insertOne(this)
+    }
+    return op
+    .catch(err=>{
+      console.log(err);
     })
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products').find().toArray()
+    .then(products=>{
+      return products
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+
+  static findById(prodId){
+    const db = getDb();
+    return db.collection('products').find({_id: new mongoDB.ObjectId(prodId)}).next()
+    .then(product=>{
+      return product
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+
+  static remove(prodId){
+    const db = getDb();
+    return db.collection('products').deleteOne({_id: new mongoDB.ObjectId(prodId)})
     .catch(err=>{
       console.log(err);
     })
